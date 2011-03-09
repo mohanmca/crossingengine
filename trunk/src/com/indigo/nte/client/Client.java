2be3fad83ddb8461eb0d8a104af005ce1cacc0be
mohanmca@gmail.com <mohanmca@gmail.com@fb7d4530-e6cb-4e31-b6a5-f628de59993e>
2011-03-09 05:07:18 +0000

    TestCases added. Logger file updated.

--- trunk/src/com/indigo/nte/client/Client.java
+++ trunk/src/com/indigo/nte/client/Client.java
@@ -1,17 +1,29 @@
 package com.indigo.nte.client;
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+
 import com.indigo.nte.exchange.Exchange;
+import com.indigo.nte.executor.Command;
+import com.indigo.nte.executor.CommandExecutor;
 import com.indigo.nte.order.Order;
 import com.indigo.nte.order.OrderGenerator;
 
 public class Client {
+	
+	static final Logger LOGGER = Logger.getLogger(Client.class);
+	
 	public static void main(String... args) {
-		OrderGenerator generator = OrderGenerator.getInstance();
-		Exchange exchange = Exchange.getInstance();
-		while (true) {
-			Order ord = generator.getNext();
-			System.out.println(ord.toString());
-			exchange.placeOrder(ord);
-		}
+		Command placeOrderCommand = new Command() {
+			@Override
+			public void execute() {
+				Order ord = OrderGenerator.getInstance().getNext();
+				LOGGER.debug(ord.toString());
+				Exchange.getInstance().placeOrder(ord);
+			}
+		};
+		CommandExecutor.INSTANCE.executeFor(5, TimeUnit.SECONDS, placeOrderCommand);
 	}
+
 }
